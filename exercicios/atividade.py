bbfc2361fcec86b6401760afb4c7429b94c80c13
Raul Andrade <raul.sandrade@hotmail.com>
2025-06-10 15:39:00 -0300

    Conclusão dos exercicios

--- exercicios/atividade.py
+++ exercicios/atividade.py
@@ -1,21 +1,3 @@
-#64 - Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
-#  Um número primo é aquele que é divisível somente por ele mesmo e por 1.
-
-numero = int(input("Informe um número "))
-resultado = 0
-primo = True
-for i in range(2,numero):
-    resultado = numero % i
-    if resultado == 0:
-        primo = False
-        break;
-    else:
-        primo = True
-
-if primo == True:
-    print("O número é primo")
-else: 
-    print("O número não é primo")
-    
-
-    
+#77 - Os números primos possuem várias aplicações dentro da Computação, por exemplo na Criptografia.
+#  Um número primo é aquele que é divisível apenas
+#  por um e por ele mesmo. Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
